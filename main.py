b6b189edcb7566522668c8927a03f96ab27fd4dd
BenjaminC62 <benjamincornet62130@gmail.com>
2025-02-01 00:27:43 +0100

    :tada: - ajout de la possibilité de marcher vers la boite du choix du starter

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import time
 import cv2
 import numpy as np
 from mouseinfo import screenshot
+from pyautogui import press
 
 key_skip = "shift"
 endroit_a_check = (277, 250, 365, 279) # (X, Y, Largeur, Hauteur)
 starter_reference = cv2.imread("pokemon.png", cv2.IMREAD_GRAYSCALE)
 game = "https://www.fandesjeux.com/jouer/pokemon-noir"
+cord_play = (468,600)
+cord_play_download = (468,820)
 
 def load_savestate():
     """loads the savestate of the game"""
@@ -22,11 +25,11 @@ def is_starter_screen_visible():
     screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2GRAY)
 
     # Verif pour ecran noir ou blanc
-    if np.all(screenshot == 0):
+    if np.all(screenshot == 0 and screenshot == 1 and screenshot == 2 and screenshot == 3):
         print("L'écran est tout noir.")
         return False
 
-    if np.all(screenshot == 255):
+    if np.all(screenshot == 255 and screenshot == 254 and screenshot == 253 and screenshot == 252):
         print("L'écran est tout blanc.")
         return False
 
@@ -58,6 +61,12 @@ def is_shiny():
     """checks if the pokemon is shiny"""
     screenshot = pyautogui.screenshot(region=endroit_a_check)
 
+
+def click_play(cord):
+    """clicks the play button"""
+    pyautogui.click(cord)
+    time.sleep(10)
+
 def open_firefox_and_game():
     """Open firefox and type in the search bar the url of the game"""
     pyautogui.hotkey('win', 'r')
@@ -65,6 +74,7 @@ def open_firefox_and_game():
     pyautogui.typewrite('firefox\n')
     time.sleep(2)
     pyautogui.typewrite(game + '\n')
+    time.sleep(3)
 
 def main():
     """execute the main program, which is a while loop that takes screenshots and saves them to a folder of a non shiny pokemon on the first run
@@ -73,13 +83,24 @@ def main():
     # Cheat shiny 521A96D0 1C221C39
 
     open_firefox_and_game()
+    click_play(cord_play)
+    click_play(cord_play_download)
 
-    time.sleep(5)
+    pyautogui.moveTo(cord_play)
 
+    time.sleep(15)
+
+    start_time = time.time()
     while not is_starter_screen_visible():
         pyautogui.keyDown("shift")
         time.sleep(0.2)
         pyautogui.keyUp("shift")
+        print("Voci les sec = ", time.time() - start_time)
+        if time.time() - start_time > 240:
+            pyautogui.keyDown("s")
+            time.sleep(2)
+            pyautogui.keyUp("s")
+
 
     time.sleep(5)
 
