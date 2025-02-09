2935a9714f394a723a9e09706f90b42d9afd7b59
BenjaminC62 <benjamincornet62130@gmail.com>
2025-02-09 15:08:29 +0100

    :memo: ajout du support de plusieur os + ajout de la création et chargement de savestate automatiquement

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+
 import pyautogui
 import time
 import cv2
 import numpy as np
+import platform
+import locale
 from mouseinfo import screenshot
 from pyautogui import press
 
@@ -17,6 +21,18 @@ def load_savestate():
     pyautogui.keyDown("shift") # hold shift
     pyautogui.press("f4")
     pyautogui.keyUp("shift")
+    time.sleep(2)
+    pyautogui.click(242, 341)
+
+def create_savestate():
+    """creates a savestate of the game"""
+    pyautogui.keyDown("shift") # hold shift
+    pyautogui.press("f2")
+    pyautogui.keyUp("shift")
+    time.sleep(2)
+    pyautogui.click(465, 535)
+    time.sleep(2)
+
 
 def is_starter_screen_visible():
     """Vérifie si l'écran de sélection du starter est affiché"""
@@ -78,17 +94,20 @@ def click_play(cord):
 
 def open_firefox_and_game():
     """Open firefox and type in the search bar the url of the game"""
-    pyautogui.hotkey('win', 'r')
+    if platform.system() == "Windows":
+        pyautogui.hotkey('win', 'r')
+    else:
+        pyautogui.hotkey('ctrl', 'alt', 't')
     time.sleep(1)
     pyautogui.typewrite('firefox\n')
     time.sleep(2)
-    pyautogui.typewrite(game + '\n')
+    if locale.getlocale()[0] == "fr_FR":
+        pyautogui.typewrite(game + '\n')
     time.sleep(3)
 
 def main():
     """execute the main program, which is a while loop that takes screenshots and saves them to a folder of a non shiny pokemon on the first run
     then it will compare the screenshots to the original image to see if the pokemon is shiny or not, then repeat the process if the pokemon is not shiny"""
-    #load_savestate()
     # Cheat shiny 521A96D0 1C221C39
 
     open_firefox_and_game()
@@ -105,9 +124,11 @@ def main():
         time.sleep(0.2)
         pyautogui.keyUp("shift")
         print("Voci les sec = ", time.time() - start_time)
-        if time.time() - start_time > 225:
-            pyautogui.keyDown("s")
+        if time.time() - start_time > 10:
+            create_savestate()
             time.sleep(2)
+            pyautogui.keyDown("s")
+            time.sleep(444)
             pyautogui.keyUp("s")
 
 
